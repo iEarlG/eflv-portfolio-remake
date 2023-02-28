0406ef6484f06611c8d9eaf44e6f9b9075ed593b
Earl Villapaz <jasonburning16@gmail.com>
2023-02-28 15:28:49 +0800

    second backend commits

---
+++ backend/schemas/index.js
@@ -0,0 +1,19 @@
+import about from "./about"
+import brands from "./brands"
+import contact from "./contact"
+import experience from "./experience"
+import skills from "./skills"
+import testimonials from "./testimonials"
+import workExperience from "./workExperience"
+import works from "./works"
+
+export const schemaTypes = [
+    about,
+    brands,
+    contact,
+    experience,
+    skills,
+    testimonials,
+    workExperience,
+    works,
+]

7570eb60e7a24a06e701e0abdff8d07e8803fb4d
Earl Villapaz <jasonburning16@gmail.com>
2023-03-03 18:57:57 +0800

    just adding final logo maybe?

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
 import { navLinks } from '../../constants/data';
@@ -6,6 +8,7 @@ import { navLinks } from '../../constants/data';
 import './Navbar.scss';
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -22,6 +25,25 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className="app__navbar-menu">
+          <HiMenuAlt4 onClick={() => setToggle(true)} />
+            {toggle && (
+              <motion.div
+                whileInView={{ x: [300, 0] }}
+                transition={{ duration: 0.85, ease: 'easeOut' }}
+              >
+                <HiX onClick={() => setToggle(false)} />
+                {navLinks.map((nav) => (
+                  <li
+                    key={nav.id}
+                  >
+                    <a href={`#${nav.id}`} onClick={() => setToggle(false)}>{nav.title}</a>
+                  </li>
+                ))}
+              </motion.div>
+            )}
+      </div>
     </nav>
   )
 }

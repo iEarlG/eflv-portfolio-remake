367541688bf4139240c34012464ecae41e41c35d
Earl Villapaz <jasonburning16@gmail.com>
2023-03-04 17:12:41 +0800

    adding hero-img, and doing the HEADER SEC

--- client/src/constants/images.js
+++ client/src/constants/images.js
@@ -1,8 +1,10 @@
 import EAVLogo from '../assets/EAV-logo.png';
+import earlHERO from '../assets/MEarl.png';
 
 import abstractBG from '../assets/abstractBG.jpg';
 
 export default { 
     EAVLogo,
+    earlHERO,
     abstractBG
 }

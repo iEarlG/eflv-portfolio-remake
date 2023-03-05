58a14ed718e02d3fb3183c2ae058bafa3cf6daeb
Earl Villapaz <jasonburning16@gmail.com>
2023-03-05 14:50:17 +0800

    polishing the HERO Section

--- client/src/constants/images.js
+++ client/src/constants/images.js
@@ -1,10 +1,21 @@
 import EAVLogo from '../assets/EAV-logo.png';
 import earlHERO from '../assets/MEarl.png';
+import git from '../assets/git.png';
+import javascript from '../assets/javascript.png';
+import react from '../assets/react.png';
+import bgIMG from '../assets/bgIMG.png';
 
 import abstractBG from '../assets/abstractBG.jpg';
+
+import circle from '../assets/circle.svg';
 
 export default { 
     EAVLogo,
     earlHERO,
-    abstractBG
+    git,
+    javascript,
+    react,
+    bgIMG,
+    abstractBG,
+    circle
 }
--- client/src/containers/Header/Header.jsx
+++ client/src/containers/Header/Header.jsx
@@ -4,23 +4,69 @@ import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import './Header.scss';
 
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: "easeInOut",
+    }
+  }
+}
+
 const Header = () => {
   return (
-    <div className="app__header app__flex">
+    <div id="home" className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
         className="app__header-info"
       >
         <div className="app__header-badge">
-          <div className="bage-cmp app__flex">
-            <span>🤙</span>
+          <div className="badge-cmp app__flex">
+            <span>👋🏽</span>
             <div style={{ marginLeft: 20 }}>
-              <p className="p__text">Hello, I am</p>
-              <h1 className="head_text">EARL</h1>
+              <p className="p-text">Hello, I am</p>
+              <h1 className="head-text">EARL</h1>
             </div>
           </div>
+
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Junior Web Developer</p>
+            <p className="p-text">Computer Literate</p>
+          </div>
         </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={images.earlHERO} alt="profile_bg" />
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="overlay_circle"
+          src={images.circle}
+          alt="profile_circle"
+        />
+      </motion.div>
+
+      <motion.div
+        variants={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {[images.javascript, images.react, images.git].map((circle, i) => (
+          <div
+            className="circle-cmp app__flex"
+            key={`circle-${i}`}
+          >
+            <img src={circle} alt="circle" />
+          </div>
+        ))}
       </motion.div>
     </div>
   )

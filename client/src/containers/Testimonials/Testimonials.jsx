aa656aef2511711f739af82dc58c9f0cd7c202c0
Earl Villapaz <jasonburning16@gmail.com>
2023-03-11 14:36:48 +0800

    testimonials img is solve

--- client/src/containers/Testimonials/Testimonials.jsx
+++ client/src/containers/Testimonials/Testimonials.jsx
@@ -9,47 +9,50 @@ import './Testemonials.scss';
 
 const Testimonials = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [testimonials, setTestimonials] = useState([]);
+  const [testimonial, setTestimonial] = useState([]);
   const [brands, setBrands] = useState([]);
 
-  const handleClick = (index) => {
-    setCurrentIndex(index);
+  const handleClick = (i) => {
+    setCurrentIndex(i);
   };
 
   useEffect(() => {
     const query = '*[_type == "testimonials"]';
     const brandsQuery = '*[_type == "brands"]';
 
-    client.fetch(query).then((data) => {
-      setTestimonials(data);
+    client.fetch(query)
+      .then((data) => {
+        setTestimonial(data);
     });
 
-    client.fetch(brandsQuery).then((data) => {
-      setBrands(data);
+    client.fetch(brandsQuery)
+      .then((data) => {
+        setBrands(data);
     });
+
   }, []);
 
   return (
     <>
-      {testimonials.length && (
+      {testimonial.length && (
         <>
           <div className="app__testimonial-item app__flex">
-            <img src={urlFor(testimonials[currentIndex].imgurl).url} alt={testimonials[currentIndex].name} />
+            <img src={urlFor(testimonial[currentIndex].imageUrl).url()} alt="tesimonials" />
             <div className="app__testimonial-content">
-              <p className="p-text">{testimonials[currentIndex].feedback}</p>
+              <p className="p-text">{testimonial[currentIndex].feedback}</p>
               <div>
-                <h4 className="bold-text">{testimonials[currentIndex].name}</h4>
-                <h5 className="p-text">{testimonials[currentIndex].company}</h5>
+                <h4 className="bold-text">{testimonial[currentIndex].name}</h4>
+                <h5 className="p-text">{testimonial[currentIndex].company}</h5>
               </div>
             </div>
           </div>
 
           <div className="app__testimonial-btns app__flex">
-            <div className="app__flex" onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
+            <div className="app__flex" onClick={() => handleClick(currentIndex === 0 ? testimonial.length - 1 : currentIndex - 1)}>
               <HiChevronLeft />
             </div>
 
-            <div className="app__flex" onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}>
+            <div className="app__flex" onClick={() => handleClick(currentIndex === testimonial.length - 1 ? 0 : currentIndex + 1)}>
               <HiChevronRight />
             </div>
           </div>
--- client/src/containers/index.js
+++ client/src/containers/index.js
@@ -2,7 +2,7 @@ import About from "./About/About";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import Skills from "./Skills/Skills";
-import Testimonials from "./Testimonials/Testemonials";
+import Testimonials from "./Testimonials/Testimonials";
 import Works from "./Works/Works";
 
 export { 
